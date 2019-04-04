27f9caed65ef071b1ed1607dd6c57e5231d9ff04
pstrzalk <p.strzalkowski@visuality.pl>
2019-04-05 01:06:32 +0200

    Handle AUTH command & Add test coverage
    Created sessionHandler to reflect reference project and be able to test easily.
    
    After code review:
    - Allowed empty password
    - Moved session authorization check to each HandleX func in sessionHandler

---
+++ session_handler.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/textproto"
+	"strings"
+
+	"github.com/google/shlex"
+)
+
+type sessionHandler struct {
+	connection io.ReadWriteCloser
+	memory     map[string]string
+	password   string
+	authorized bool
+}
+
+func (s *sessionHandler) handleConnection() {
+	defer s.connection.Close()
+	reader := bufio.NewReader(s.connection)
+
+	for {
+		finish, _ := s.handleSingleConnection(reader)
+		if finish {
+			break
+		}
+	}
+}
+
+func (s *sessionHandler) handleSingleConnection(reader *bufio.Reader) (bool, error) {
+	tpReader := textproto.NewReader(reader)
+
+	netData, err := tpReader.ReadLine()
+	if err != nil {
+		s.RespondWithError("Protocol error: " + err.Error())
+		return false, err
+	}
+
+	command := strings.TrimSpace(string(netData))
+	if command == "" {
+		return false, nil
+	}
+
+	commandToLower := strings.ToLower(command)
+	if commandToLower == "quit" {
+		s.RespondPlain("+OK")
+		return true, nil
+	}
+
+	commandSplit, err := shlex.Split(command)
+	if err != nil {
+		s.RespondWithError(err.Error())
+		return false, err
+	}
+
+	s.Respond(commandSplit)
+	return false, nil
+}
+
+func (s *sessionHandler) Respond(commandSplit []string) {
+	baseCommand := strings.ToLower(commandSplit[0])
+
+	if baseCommand == "auth" {
+		s.HandleAuth(commandSplit)
+	} else if baseCommand == "set" {
+		s.HandleSet(commandSplit)
+	} else if baseCommand == "get" {
+		s.HandleGet(commandSplit)
+	} else if baseCommand == "del" {
+		s.HandleDel(commandSplit)
+	} else if baseCommand == "ping" {
+		s.HandlePing(commandSplit)
+	} else if len(baseCommand) > 0 {
+		s.RespondWithError(fmt.Sprintf("unknown command '%s'", baseCommand))
+	}
+}
+
+func (s *sessionHandler) HandleAuth(commandSplit []string) {
+	if len(commandSplit) > 2 {
+		s.RespondWithError("wrong number of arguments for 'auth' command")
+	} else if commandSplit[1] == s.password {
+		s.authorized = true
+		s.RespondPlain("+OK")
+	} else {
+		s.RespondWithError("invalid password")
+	}
+}
+
+func (s *sessionHandler) HandleSet(commandSplit []string) {
+	if !s.authorized {
+		s.RespondWithNoAuth()
+		return
+	}
+
+	if len(commandSplit) == 2 {
+		s.RespondWithError("wrong number of arguments for 'set' command")
+	} else {
+		s.memory[commandSplit[1]] = commandSplit[2]
+		s.RespondPlain("+OK")
+	}
+}
+
+func (s *sessionHandler) HandleGet(commandSplit []string) {
+	if !s.authorized {
+		s.RespondWithNoAuth()
+		return
+	}
+
+	if len(commandSplit) != 2 {
+		s.RespondWithError("wrong number of arguments for 'get' command")
+	} else {
+		value, ok := s.memory[commandSplit[1]]
+
+		if ok {
+			s.RespondWithValue(value)
+		} else {
+			s.RespondPlain("$-1")
+		}
+	}
+}
+
+func (s *sessionHandler) HandleDel(commandSplit []string) {
+	if !s.authorized {
+		s.RespondWithNoAuth()
+		return
+	}
+
+	if len(commandSplit) == 1 {
+		s.RespondWithError("wrong number of arguments for 'del' command")
+	} else {
+		numberOfDeletes := 0
+
+		for i := 1; i < len(commandSplit); i++ {
+			_, ok := s.memory[commandSplit[i]]
+			if ok {
+				numberOfDeletes += 1
+			}
+			delete(s.memory, commandSplit[i])
+		}
+		s.RespondPlain(fmt.Sprintf(":%d", numberOfDeletes))
+	}
+}
+
+func (s *sessionHandler) HandlePing(commandSplit []string) {
+	if !s.authorized {
+		s.RespondWithNoAuth()
+		return
+	}
+
+	if len(commandSplit) > 2 {
+		s.RespondWithError("wrong number of arguments for 'ping' command")
+	} else if len(commandSplit) == 2 {
+		s.RespondWithValue(commandSplit[1])
+	} else {
+		s.RespondPlain("+PONG")
+	}
+}
+
+func (s *sessionHandler) RespondWithNoAuth() {
+	message := "-NOAUTH Authentication required.\n"
+	s.connection.Write([]byte(message))
+}
+
+func (s *sessionHandler) RespondWithError(response string) {
+	message := fmt.Sprintf("-ERR %s\n", response)
+	s.connection.Write([]byte(message))
+}
+
+func (s *sessionHandler) RespondWithValue(response string) {
+	message := fmt.Sprintf("$%d\n", len(response))
+	s.connection.Write([]byte(message))
+	s.RespondPlain(response)
+}
+
+func (s *sessionHandler) RespondPlain(response string) {
+	s.connection.Write([]byte(response + "\n"))
+}
---
+++ session_handler_test.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type CloseableBuffer struct {
+	bytes.Buffer
+}
+
+func (b *CloseableBuffer) Close() error {
+	return nil
+}
+
+func TestHandleSingleConnection(t *testing.T) {
+	buffer := CloseableBuffer{}
+	s := sessionHandler{connection: &buffer, memory: nil, authorized: false}
+
+	reader := bufio.NewReader(s.connection)
+
+	buffer.WriteString("ping")
+	s.handleSingleConnection(reader)
+	assert.Equal(t, "-NOAUTH Authentication required.\n", buffer.String())
+	buffer.Reset()
+
+	s.authorized = true
+
+	buffer.WriteString("\n")
+	ret, err := s.handleSingleConnection(reader)
+	assert.Equal(t, false, ret)
+  assert.Nil(t, err)
+
+	buffer.WriteString("")
+	s.handleSingleConnection(reader)
+	assert.Equal(t, "-ERR Protocol error: EOF\n", buffer.String())
+	buffer.Reset()
+
+	buffer.WriteString("set \"a b")
+	s.handleSingleConnection(reader)
+	assert.Equal(t, "-ERR EOF found when expecting closing quote\n", buffer.String())
+	buffer.Reset()
+
+	buffer.WriteString("quit")
+	s.handleSingleConnection(reader)
+	assert.Equal(t, "+OK\n", buffer.String())
+}
+
+func TestRespondWithNoAuth(t *testing.T) {
+	buffer := CloseableBuffer{}
+	s := sessionHandler{connection: &buffer, memory: nil}
+
+	s.RespondWithNoAuth()
+	assert.Equal(t, "-NOAUTH Authentication required.\n", buffer.String())
+}
+
+func TestHandleAuth(t *testing.T) {
+	buffer := CloseableBuffer{}
+	s := sessionHandler{connection: &buffer, memory: nil, password: "PASS"}
+
+	s.HandleAuth([]string{"auth", "b", "c"})
+	assert.Equal(t, "-ERR wrong number of arguments for 'auth' command\n", buffer.String())
+	buffer.Reset()
+
+	s.HandleAuth([]string{"auth", "BADPASS"})
+	assert.Equal(t, "-ERR invalid password\n", buffer.String())
+	buffer.Reset()
+
+	s.HandleAuth([]string{"auth", "PASS"})
+	assert.Equal(t, "+OK\n", buffer.String())
+	buffer.Reset()
+}
+
+func TestHandleSet(t *testing.T) {
+	buffer := CloseableBuffer{}
+	memory := make(map[string]string)
+
+	s := sessionHandler{connection: &buffer, memory: memory, authorized: false}
+
+	s.HandleSet([]string{"SET", "key", "val"})
+	assert.Equal(t, "-NOAUTH Authentication required.\n", buffer.String())
+	buffer.Reset()
+
+	s.authorized = true
+
+	s.HandleSet([]string{"set", "b"})
+	assert.Equal(t, "-ERR wrong number of arguments for 'set' command\n", buffer.String())
+	buffer.Reset()
+
+	s.HandleSet([]string{"SET", "key", "val"})
+	assert.Equal(t, "+OK\n", buffer.String())
+	assert.Equal(t, "val", memory["key"])
+	buffer.Reset()
+}
+
+func TestHandleGet(t *testing.T) {
+	buffer := CloseableBuffer{}
+	memory := make(map[string]string)
+	memory["key"] = "val"
+
+	s := sessionHandler{connection: &buffer, memory: memory, authorized: false}
+
+	s.HandleGet([]string{"get", "key"})
+	assert.Equal(t, "-NOAUTH Authentication required.\n", buffer.String())
+	buffer.Reset()
+
+	s.authorized = true
+
+	s.HandleGet([]string{"get", "a", "b"})
+	assert.Equal(t, "-ERR wrong number of arguments for 'get' command\n", buffer.String())
+	buffer.Reset()
+
+	s.HandleGet([]string{"get", "key"})
+	assert.Equal(t, "$3\nval\n", buffer.String())
+	buffer.Reset()
+
+	s.HandleGet([]string{"get", "wrongkey"})
+	assert.Equal(t, "$-1\n", buffer.String())
+	buffer.Reset()
+}
+
+func TestHandleDel(t *testing.T) {
+	buffer := CloseableBuffer{}
+	memory := make(map[string]string)
+	memory["key"] = "val"
+
+	s := sessionHandler{connection: &buffer, memory: memory, authorized: false}
+
+	s.HandleDel([]string{"del", "key"})
+	assert.Equal(t, "-NOAUTH Authentication required.\n", buffer.String())
+	buffer.Reset()
+
+	s.authorized = true
+
+	s.HandleDel([]string{"del"})
+	assert.Equal(t, "-ERR wrong number of arguments for 'del' command\n", buffer.String())
+	buffer.Reset()
+
+	s.HandleDel([]string{"del", "key"})
+	assert.Equal(t, ":1\n", buffer.String())
+	value, err := memory["key"]
+	assert.Equal(t, "", value)
+	assert.NotNil(t, err)
+	buffer.Reset()
+}
+
+func TestPing(t *testing.T) {
+	buffer := CloseableBuffer{}
+
+	s := sessionHandler{connection: &buffer, memory: nil, authorized: false}
+
+	s.HandlePing([]string{"ping"})
+	assert.Equal(t, "-NOAUTH Authentication required.\n", buffer.String())
+	buffer.Reset()
+
+	s.authorized = true
+
+	s.HandlePing([]string{"ping", "a", "b"})
+	assert.Equal(t, "-ERR wrong number of arguments for 'ping' command\n", buffer.String())
+	buffer.Reset()
+
+	s.HandlePing([]string{"ping", "abc"})
+	assert.Equal(t, "$3\nabc\n", buffer.String())
+	buffer.Reset()
+
+	s.HandlePing([]string{"ping"})
+	assert.Equal(t, "+PONG\n", buffer.String())
+	buffer.Reset()
+}
+
+func TestRespond(t *testing.T) {
+	buffer := CloseableBuffer{}
+	memory := make(map[string]string)
+
+	s := sessionHandler{connection: &buffer, memory: memory, password: "PASS", authorized: false}
+	s.Respond([]string{"get"})
+	assert.Equal(t, "-NOAUTH Authentication required.\n", buffer.String())
+	buffer.Reset()
+
+	s.Respond([]string{"auth", "PASS"})
+	assert.Equal(t, "+OK\n", buffer.String())
+	buffer.Reset()
+
+	s.Respond([]string{"set", "key", "val"})
+	assert.Equal(t, "+OK\n", buffer.String())
+	buffer.Reset()
+
+	s.Respond([]string{"get", "key"})
+	assert.Equal(t, "$3\nval\n", buffer.String())
+	buffer.Reset()
+
+	s.Respond([]string{"del", "key"})
+	assert.Equal(t, ":1\n", buffer.String())
+	buffer.Reset()
+
+	s.Respond([]string{"ping"})
+	assert.Equal(t, "+PONG\n", buffer.String())
+	buffer.Reset()
+
+	s.Respond([]string{"wrongcommand"})
+	assert.Equal(t, "-ERR unknown command 'wrongcommand'\n", buffer.String())
+	buffer.Reset()
+}
